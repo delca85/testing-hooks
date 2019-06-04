c65b0f4954a0b51b27cb7d1c50187cbea17967cb
Bianca Del Carretto <bianca.delcarretto@wyscout.com>
2019-06-04 16:30:20 +0200

    fix mocked DisplayData

--- src/components/__tests__/DisplayDataContainer.test.js
+++ src/components/__tests__/DisplayDataContainer.test.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { render, fireEvent } from "react-testing-library";
 import DisplayDataContainer from "../DisplayDataContainer";
+import DisplayData from "../DisplayData";
 
-jest.mock("../DisplayData", () => ({ query }) => (
-    <div data-testid='displayData'>{query}</div>
-));
+jest.mock("../DisplayData", () => ({
+    __esModule: true,
+    default: jest.fn(() => <div />),
+}));
 
 describe("DisplayDataContainer Component", () => {
     it("should set prop right to DisplayData if a new input is written", () => {
-        const { container, getByTestId } = render(<DisplayDataContainer />);
+        const { container } = render(<DisplayDataContainer />);
         const inputValue = "pippo";
         const input = container.querySelector("input");
         fireEvent.change(input, { target: { value: inputValue } });
-        expect(getByTestId("displayData")).toHaveTextContent(inputValue);
+
+        expect(DisplayData).toBeCalledWith({ query: inputValue }, {});
     });
 });
